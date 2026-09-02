cbe00fab9a192c380f00fce4b1cb79a604c08f2d
Parthasarathy AK <parthasarathy.ak@zohocorp.com>
2026-09-02 14:37:39 +0530

    add: methods to debug lifecycle

--- app/src/main/java/com/example/expensetracker/ExpenseFormActivity.kt
+++ app/src/main/java/com/example/expensetracker/ExpenseFormActivity.kt
@@ -79,4 +79,26 @@ class ExpenseFormActivity : AppCompatActivity() {
         )
         datePickerDialog.show()
     }
+    override fun onStart() {
+        super.onStart()
+    }
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
 }
--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -33,8 +33,28 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, ExpenseFormActivity::class.java))
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
     override fun onResume() {
         super.onResume()
         expenseAdapter.notifyDataSetChanged()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
